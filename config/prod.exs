70e7353a53418852ee190335d4d103e774d29096
Nicolas Müller <nicolas.mueller2@gmail.com>
2025-10-16 15:33:44 +0200

    fix host

--- config/prod.exs
+++ config/prod.exs
@@ -6,8 +6,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :photoguessr, PhotoguessrWeb.Endpoint,
-  cache_static_manifest: "priv/static/cache_manifest.json",
-  url: [host: "photoguessr.coffee-journal.com"]
+  cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Configures Swoosh API Client
 config :swoosh, api_client: Swoosh.ApiClient.Req
